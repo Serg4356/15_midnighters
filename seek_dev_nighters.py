2216086fc6991f52d82ae7be9d8431803d4acd45
Serg4356 <asdfg111000@yandex.ru>
2018-01-25 09:36:16 +0500

    Add requierements, simplify code, removw excessive function

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,17 +1,15 @@
 import requests
 import datetime
 import pytz
-import collections
 
 
 def load_attempts():
-    response = requests.get(
-        'https://devman.org/api/challenges/solution_attempts')
-    pages = int(response.json()['number_of_pages'])
-    for page in range(pages):
+    for page in range(1, int(requests.get(
+        'https://devman.org/api/challenges/solution_attempts'
+    ).json()['number_of_pages']) + 1, 1):
         page_response = requests.get(
             'https://devman.org/api/challenges/solution_attempts',
-            params={'page': int(page)+1}
+            params={'page': page}
         ).json()
         for record in page_response['records']:
             yield {
@@ -22,31 +20,24 @@ def load_attempts():
 
 
 def get_midnighters():
-    timezone_stamp = pytz.timezone('Europe/Moscow')
+    midnight = 0
+    morning = 0
     midnighters = []
     for attempt in load_attempts():
-        hour_of_attempt = to_hours_in_one_tz(
-            timestamp_to_timedate(attempt['timestamp']),
-            timezone_stamp,
-            pytz.timezone(attempt['timezone']))
-        if 0 <= int(hour_of_attempt) <= 5:
+        hour_of_attempt = timestamp_to_timedate(attempt['timestamp'])
+        if midnight <= int(hour_of_attempt) <= morning:
             midnighters.append(attempt['username'])
     return midnighters
 
 
 def timestamp_to_timedate(timestamp):
-    return datetime.datetime.fromtimestamp(timestamp)
-
-
-def to_hours_in_one_tz(time, timezone_stamp, timezone_sample):
-    return timezone_sample.localize(
-        time
-    ).astimezone(
-        timezone_stamp
-    ).strftime('%H')
+    return datetime.datetime.fromtimestamp(
+        timestamp,
+        tz=pytz.timezone('Europe/Moscow')
+    ).hour
 
 
 if __name__ == '__main__':
     print('Midnighters:')
-    for midnighter in collections.Counter(get_midnighters()):
+    for midnighter in set(get_midnighters()):
         print(midnighter)
